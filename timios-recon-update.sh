7a2beb4be8d392536bf2585c6b5f9f683a12fc8d
Jason Cardinal <jason.cardinal@timiosdevelopment.com>
2021-01-04 15:41:11 -0600

    updates

---
+++ timios-recon-update.sh
@@ -0,0 +1,24 @@
+
+if [ -z "$1" ]; then
+  echo "Github access token needed"
+  exit 1
+fi
+
+TOKEN=$1
+OWNER="timiosdevelopment"
+REPO="atw-pineapple"
+
+download_file() {
+  local file="https://api.github.com/repos/$OWNER/$REPO/contents/$1"
+
+  echo "$file"
+
+  curl --header "Authorization: token $TOKEN" \
+       --header "Accept: application/vnd.github.v3.raw" \
+       --remote-name \
+       --location $file
+}
+
+
+download_file "recon.py"
+download_file "timios-recon"
