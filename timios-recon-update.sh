169f16ca7dc6fa30d5d639d5c39e3e8a8f8916c1
Jason Cardinal <jason.cardinal@timiosdevelopment.com>
2021-02-05 14:20:29 -0600

    Update timios-recon-update.sh

--- timios-recon-update.sh
+++ timios-recon-update.sh
@@ -5,7 +5,7 @@ CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 
 # Check dependencies.
 set -e
-type curl grep sed tr >&2
+type curl grep sed tr jq >&2
 xargs=$(which gxargs || which xargs)
 
 # Validate settings.
@@ -17,26 +17,16 @@ xargs=$(which gxargs || which xargs)
 NAME="recon-upload-mipsel"
 OWNER="timiosdevelopment"
 REPO="atw-pineapple"
-TAG="1.0.0"
 GH_API="https://api.github.com"
 GH_REPO="$GH_API/repos/$OWNER/$REPO"
-GH_TAGS="$GH_REPO/releases/tags/$TAG"
+GH_TARBALL="$GH_REPO/tarball/1.0.0"
 AUTH="Authorization: token $GITHUB_API_TOKEN"
-WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
 CURL_ARGS="-LJO#"
 
 # Validate token.
 curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
 
-# Read asset tags.
-response=$(curl -sH "$AUTH" $GH_TAGS)
-# Get ID of the asset based on given name.
-# eval $(echo "$response" | grep -C3 "name.:.\+$NAME" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
-id=$(echo "$response" | jq --arg name "$NAME" '.assets[] | select(.name == $name).id') # If jq is installed, this can be used instead. 
-[ "$id" ] || { echo "Error: Failed to get asset id, response: $response" | awk 'length($0)<100' >&2; exit 1; }
-GH_ASSET="$GH_REPO/releases/assets/$id"
-
 # Download asset file.
 echo "Downloading asset..." >&2
-curl $CURL_ARGS -H "Authorization: token $GITHUB_API_TOKEN" -H 'Accept: application/octet-stream' "$GH_ASSET"
+curl $CURL_ARGS -H "$AUTH" -H 'Accept: application/octet-stream' "$GH_TARBALL"
 echo "$0 done." >&2
