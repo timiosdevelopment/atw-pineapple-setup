53a55b56e020eef0ae2c2e35d66cadd974c93717
Jason Cardinal <jason.cardinal@timiosdevelopment.com>
2021-02-05 13:59:12 -0600

    Update timios-recon-update.sh

--- timios-recon-update.sh
+++ timios-recon-update.sh
@@ -8,7 +8,7 @@ TOKEN=$1;
 OWNER="timiosdevelopment";
 REPO="atw-pineapple";
 
-auth_header="--header='Authorization: token $TOKEN'";
+auth_header="-d --header='Authorization: token $TOKEN'";
 accept_header="--header='Accept:application/octet-stream'";
 ASSET_ID=$(wget -d "$auth_header" -O - https://api.github.com/repos/$OWNER/$REPO/releases/tags/latest | python3 -c 'import sys, json; print json.load(sys.stdin)["assets"][0]["id"]') && \
-  wget -d "$auth_header" "$accept_header" O timios-recon.tar.gz https://api.github.com/repos/$OWNER/$REPO/releases/assets/$ASSET_ID;
+  wget "$auth_header" "$accept_header" -O timios-recon.tar.gz https://api.github.com/repos/$OWNER/$REPO/releases/assets/$ASSET_ID;
