332f925dd898533b33b146d506a5051a8d4d6c6a
Jason Cardinal <jason.cardinal@timiosdevelopment.com>
2021-02-05 13:23:22 -0600

    Update timios-recon-update.sh

--- timios-recon-update.sh
+++ timios-recon-update.sh
@@ -4,13 +4,12 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-TOKEN=$1
-OWNER="timiosdevelopment"
-REPO="atw-pineapple"
+TOKEN=$1;
+OWNER="timiosdevelopment";
+REPO="atw-pineapple";
 
-CURL="curl -H 'Authorization: token $TOKEN' \
-      https://api.github.com/repos/$OWNER/$REPO/releases"; \
-ASSET_ID=$(eval "$CURL/tags/latest" | jq .assets[0].id); \
-eval "$CURL/assets/$ASSET_ID -LJOH 'Accept: application/octet-stream'"
+CURL="curl -H 'Authorization: token $TOKEN' https://api.github.com/repos/$OWNER/$REPO/releases";
+ASSET_ID=$(eval "$CURL/tags/latest" | jq .assets[0].id);
+eval "$CURL/assets/$ASSET_ID -LJOH 'Accept: application/octet-stream'";
 
 mkdir -p /root/timios-recon/
