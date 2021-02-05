71988940c7b28eeaad9693b6565452cd8c6dc615
Jason Cardinal <jason.cardinal@timiosdevelopment.com>
2021-02-05 13:18:11 -0600

    Update timios-recon-update.sh

--- timios-recon-update.sh
+++ timios-recon-update.sh
@@ -8,19 +8,9 @@ TOKEN=$1
 OWNER="timiosdevelopment"
 REPO="atw-pineapple"
 
-download_file() {
-  local file="https://api.github.com/repos/$OWNER/$REPO/contents/$1"
+CURL="curl -H 'Authorization: token $TOKEN' \
+      https://api.github.com/repos/$OWNER/$REPO/releases"; \
+ASSET_ID=$(eval "$CURL/tags/<tag>" | jq .assets[0].id); \
+eval "$CURL/assets/$ASSET_ID -LJOH 'Accept: application/octet-stream'"
 
-  echo "$file"
-
-  curl --header "Authorization: token $TOKEN" \
-       --header "Accept: application/vnd.github.v3.raw" \
-       --remote-name \
-       --location $file
-}
-
-
-download_file "recon.py"
-download_file "timios-recon"
-
-chmod +x ./timios-recon
+mkdir -p /root/timios-recon/
